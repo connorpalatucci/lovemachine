fe7c485b5484506e19968bcf34a60720e8dc0528
Connor Palatucci <connor.palatucci@gmail.com>
2019-08-21 20:57:51 -0700

    updated poem generator function

--- generator.py
+++ generator.py
@@ -2,6 +2,8 @@ import markovify
 import cleaner
 import os
 import random
+import datetime
+
 
 rawCorpusFile = 'corpus_raw.txt'
 cleanedCorpusFile = 'corpus_CLEANED.txt'
@@ -16,45 +18,50 @@ with open(cleanedCorpusFile) as f:
 
 # Build the model.
 text_model = markovify.NewlineText(text, state_size=3)
+print("Model Built at:  " + str(datetime.datetime.now().time()))
+
+
+def writePoem():
+        poem = ""
+        #choose number of lines per stanza
+        lineMax = random.randint(2, 12)
+
+        #choose number of stanzas based on line num
+        if (lineMax < 5 ):
+                stanzaMax = random.randint(3, 7)
+        elif(lineMax < 11):
+                stanzaMax = 2
+        else :
+                stanzaMax = 1
+
+        print("lineMax - "+str(lineMax))
+        print("stanzaMax - "+str(stanzaMax))
+
+        for i in range(0, stanzaMax):
+                stanza = ""
+                
+                for j in range(0, lineMax):
+                        #generate lines util you get one less 
+                        #than 40 char long and not none
+                        s = None
+                        
+                        while (s is None):
+                                s = text_model.make_sentence()
+                                if s is not None:
+                                        if len(s)>50:
+                                                s=None
+                        stanza += s
+                        stanza += "\n"
+                poem +=stanza
+                poem +="\n"
+        return(poem)
 
 #Generate a poem
-poem = ""
+print("Writing Poem at:  " + str(datetime.datetime.now().time()))
+newPoem = writePoem()
 
-lineMax = random.randint(2, 12)
-
-if (lineMax < 5 ):
-	stanzaMax = random.randint(3, 7)
-elif(lineMax < 11):
-	stanzaMax = 2
-else :
-	stanzaMax = 1
-
-print("lineMax - "+str(lineMax))
-print("stanzaMax - "+str(stanzaMax))
-
-for i in range(0, stanzaMax):
-	stanza = ""
-	
-	for j in range(0, lineMax):
-		#generate lines util you get one less 
-		#than 40 char long and not none
-		s = None
-		
-		while (s is None):
-			s = text_model.make_sentence()
-			if s is not None:
-				if len(s)>50:
-					s=None
-			# print(str(j)+str(s))
-
-		stanza += s
-		stanza += "\n"
-	# print("----------Stanza-------")
-	# print(stanza)
-	# print("----------")
-	poem +=stanza
-	poem +="\n"
-print(poem)
+print("Poem Written at:  " + str(datetime.datetime.now().time()))
+print(newPoem)
 
 
 # Print five randomly-generated sentences
