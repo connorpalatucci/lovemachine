4dc1c73604f269b0e21ff2b1fac0e5a05529dcb0
Connor Palatucci <connor.palatucci@gmail.com>
2019-08-24 12:38:32 -0700

    main.py

--- adafruit main.py
+++ adafruit main.py
@@ -22,8 +22,10 @@ from Adafruit_Thermal import *
 
 ledPin       = 18
 buttonPin    = 23
-holdTime     = 4     # Duration for button hold (shutdown)
+holdTime     = 2     # Duration for button hold (shutdown)
 tapTime      = 0.01  # Debounce time for button taps
+nextInterval = 0.0   # Time of next recurring operation
+dailyFlag    = False # Set after daily trigger occurs
 lastId       = '1'   # State information passed to/from interval script
 printer      = Adafruit_Thermal("/dev/serial0", 19200, timeout=5)
 
@@ -45,6 +47,25 @@ def hold():
   GPIO.output(ledPin, GPIO.LOW)
 
 
+# Called at periodic intervals (30 seconds by default).
+# Invokes twitter script.
+def interval():
+  GPIO.output(ledPin, GPIO.HIGH)
+  p = subprocess.Popen(["python", "twitter.py", str(lastId)],
+    stdout=subprocess.PIPE)
+  GPIO.output(ledPin, GPIO.LOW)
+  return p.communicate()[0] # Script pipes back lastId, returned to main
+
+
+# Called once per day (6:30am by default).
+# Invokes weather forecast and sudoku-gfx scripts.
+def daily():
+  GPIO.output(ledPin, GPIO.HIGH)
+  subprocess.call(["python", "forecast.py"])
+  subprocess.call(["python", "sudoku-gfx.py"])
+  GPIO.output(ledPin, GPIO.LOW)
+
+
 # Initialization
 
 # Use Broadcom pin numbers (not Raspberry Pi pin numbers) for GPIO
@@ -59,7 +80,7 @@ GPIO.output(ledPin, GPIO.HIGH)
 
 # Processor load is heavy at startup; wait a moment to avoid
 # stalling during greeting.
-time.sleep(10)
+time.sleep(30)
 
 # Show IP address (if network is available)
 try:
@@ -125,4 +146,22 @@ while(True):
   else:
     GPIO.output(ledPin, GPIO.LOW)
 
+  # Once per day (currently set for 6:30am local time, or when script
+  # is first run, if after 6:30am), run forecast and sudoku scripts.
+  l = time.localtime()
+  if (60 * l.tm_hour + l.tm_min) > (60 * 6 + 30):
+    if dailyFlag == False:
+      daily()
+      dailyFlag = True
+  else:
+    dailyFlag = False  # Reset daily trigger
+
+  # Every 30 seconds, run Twitter scripts.  'lastId' is passed around
+  # to preserve state between invocations.  Probably simpler to do an
+  # import thing.
+  if t > nextInterval:
+    nextInterval = t + 30.0
+    result = interval()
+    if result is not None:
+      lastId = result.rstrip('\r\n')
 
