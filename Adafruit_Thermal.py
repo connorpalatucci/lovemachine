0b46022c619b0bdbf81a43d2717ba78ec5e7353b
Connor Palatucci <connor.palatucci@gmail.com>
2019-09-14 11:18:24 -0700

    qr fixes

--- Adafruit_Thermal.py
+++ Adafruit_Thermal.py
@@ -41,7 +41,7 @@ import sys
 class Adafruit_Thermal(Serial):
 
 	resumeTime      =   0.0
-	byteTime        =   10
+	byteTime        =   0
 	dotPrintTime    =   0
 	dotFeedTime     =   0.0
 	prevByte        =  '\n'
